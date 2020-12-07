758d49c5e70fc65202e6ed0cdbcfe4f52761b0de
Stanislav <amaterasu.dzen@gmail.com>
2020-12-07 12:23:31 +0300

    finish ZXing on Unity

---
+++ Assets/Example ZXing/Scripts/ScannerQR.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScannerQR : ZXingQR
+{
+    [SerializeField] private RawImage outRawImageQR;
+    [SerializeField] private InputField inputTextToQR;
+    [SerializeField] private RawImage outCameraWebTexture;
+    [SerializeField] private Text outDecodeText;
+    [SerializeField] private AspectRatioFitter fit;
+
+    private string textToQR;
+    private WebCamTexture webCam;
+
+    // получаем разрешение к камере 
+    private void Awake()
+    {
+        if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+            Application.RequestUserAuthorization(UserAuthorization.WebCam);
+    }
+
+    // запуск камеры устройства
+    void Start()
+    {
+        openCameraDevice();
+    }
+
+    //получаем текст для кодирования 
+    public void OnGetText()
+    {
+        textToQR = inputTextToQR.text;
+    }
+
+    // кодирование текста в QR код
+    public void OnGenerateQR()
+    {
+        outRawImageQR.texture = GenerateQR(textToQR);
+    }
+
+    // декодирование QR кода в текст
+    public void OnRegenerateQR()
+    {
+        if (ReGenerateQR(webCam) != null)
+            outDecodeText.text = ReGenerateQR(webCam);
+        else
+            outDecodeText.text = "Not found QR code";
+    }
+
+    private void openCameraDevice()
+    {
+        webCam = new WebCamTexture();
+        webCam.Play();
+
+
+        if (webCam.isPlaying)
+            outCameraWebTexture.texture = webCam;
+        else
+            Debug.LogWarning(webCam.name + "is not available");
+
+        float ratio = (float)webCam.width / (float)webCam.height;
+        fit.aspectRatio = ratio;
+        float scaleY = webCam.videoVerticallyMirrored ? -1f : 1f;
+        outCameraWebTexture.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -webCam.videoRotationAngle;
+        outCameraWebTexture.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+}
---
+++ Assets/Plugins/ZXingQR.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+public class ZXingQR : MonoBehaviour
+{
+    #region Public methods
+
+    /// <summary>
+    /// Генератор QR кода
+    /// </summary>
+    /// <param name="text"> Текст для шифрования </param>
+    /// <returns> Вернет текстуру QR </returns>
+    public Texture2D GenerateQR(string text)
+    {
+        try
+        {
+            var encoded = new Texture2D(256, 256);
+            var color32 = Encode(text, encoded.width, encoded.height);
+            encoded.SetPixels32(color32);
+            encoded.Apply();
+            return encoded;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning(ex.Message + ": text is null");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Дешифрование QR кода
+    /// </summary>
+    /// <param name="texture"> Текстура QR для дешифрования </param>
+    /// <returns> Возвращает зашифрованное сообщение </returns>
+    public string ReGenerateQR(Texture2D texture)
+    {
+        IBarcodeReader barcodeReader = new BarcodeReader();
+        var result = barcodeReader.Decode(texture.GetPixels32(),
+             texture.width, texture.height);
+
+        if (result != null)
+        {
+            Debug.Log("Decoded text from QR: " + result.Text);
+            return result.Text;
+        }
+        else
+        {
+            Debug.Log("Not found QR code");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Дешифрование QR кода
+    /// </summary>
+    /// <param name="webTexture"> Текстура QR для дешифрования </param>
+    /// <returns> Возвращает зашифрованное сообщение </returns>
+    public string ReGenerateQR(WebCamTexture webTexture)
+    {
+        IBarcodeReader barcodeReader = new BarcodeReader();
+        var result = barcodeReader.Decode(webTexture.GetPixels32(),
+             webTexture.width, webTexture.height);
+
+        if (result != null)
+        {
+            Debug.Log("Decoded text from QR: " + result.Text);
+            return result.Text;
+        }
+        else
+        {
+            Debug.Log("Not found QR code");
+            return null;
+        }
+    }
+
+    #region Encryption
+
+    /// <summary>
+    /// Кодирование текста CryptoStream
+    /// </summary>
+    /// <param name="data"> текст кодирования </param>
+    /// <param name="passPhrase"> секретный ключ </param>
+    /// <param name="saltValue"> соль </param>
+    /// <param name="hashAlgorithm"> MD5 или SHA1 </param>
+    /// <param name="passwordIterations"> любое число </param>
+    /// <param name="keySize"> может быть 192 или 128 </param>
+    /// <returns></returns>
+    public string Encrypt(string data, string passPhrase = "test", string saltValue = "test", string hashAlgorithm = "SHA1",
+        int passwordIterations = 7, int keySize = 128)
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes("~1B2c3D4e5F6g7H8");
+        byte[] rgbSalt = Encoding.ASCII.GetBytes(saltValue);
+        byte[] buffer = Encoding.UTF8.GetBytes(data);
+        byte[] rgbKey = new PasswordDeriveBytes(passPhrase, rgbSalt, hashAlgorithm, passwordIterations).GetBytes(keySize / 8);
+        RijndaelManaged managed = new RijndaelManaged();
+        managed.Mode = CipherMode.CBC;
+        ICryptoTransform transform = managed.CreateEncryptor(rgbKey, bytes);
+        MemoryStream stream = new MemoryStream();
+        CryptoStream stream2 = new CryptoStream(stream, transform, CryptoStreamMode.Write);
+        stream2.Write(buffer, 0, buffer.Length);
+        stream2.FlushFinalBlock();
+        byte[] inArray = stream.ToArray();
+        stream.Close();
+        stream2.Close();
+        return Convert.ToBase64String(inArray);
+    }
+
+    /// <summary>
+    /// Декодирование текста CryptoStream
+    /// </summary>
+    /// <param name="data"> текст кодирования </param>
+    /// <param name="passPhrase"> секретный ключ </param>
+    /// <param name="saltValue"> соль </param>
+    /// <param name="hashAlgorithm"> MD5 или SHA1 </param>
+    /// <param name="passwordIterations"> любое число </param>
+    /// <param name="keySize"> может быть 192 или 128 </param>
+    /// <returns></returns>
+    public string Decrypt(string data, string passPhrase = "test", string saltValue = "test", string hashAlgorithm = "SHA1",
+        int passwordIterations = 7, int keySize = 128)
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes("~1B2c3D4e5F6g7H8");
+        byte[] rgbSalt = Encoding.ASCII.GetBytes(saltValue);
+        byte[] buffer = Convert.FromBase64String(data);
+        byte[] rgbKey = new PasswordDeriveBytes(passPhrase, rgbSalt, hashAlgorithm, passwordIterations).GetBytes(keySize / 8);
+        RijndaelManaged managed = new RijndaelManaged();
+        managed.Mode = CipherMode.CBC;
+        ICryptoTransform transform = managed.CreateDecryptor(rgbKey, bytes);
+        MemoryStream stream = new MemoryStream(buffer);
+        CryptoStream stream2 = new CryptoStream(stream, transform, CryptoStreamMode.Read);
+        byte[] buffer5 = new byte[buffer.Length];
+        int count = stream2.Read(buffer5, 0, buffer5.Length);
+        stream.Close();
+        stream2.Close();
+        return Encoding.UTF8.GetString(buffer5, 0, count);
+    }
+    
+    #endregion
+
+    #endregion
+
+    #region Private methods
+    //encoder
+    private static Color32[] Encode(string textForEncoding, int width, int height)
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width,
+                Margin = 0
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+
+    #endregion
+}
